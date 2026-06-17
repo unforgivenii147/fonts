8af18762bff90dc234ddc1a99e6b8a9c900bc1ab
unforgivenii147 <adnanonagh@gmail.com>
2026-06-17 06:25:53 +0430

    Auto-commit at 2026-06-17 06:25:53

---
+++ initer.sh
@@ -0,0 +1,7 @@
+echo "# fonts" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/unforgivenii147/fonts.git
+git push -u origin main
